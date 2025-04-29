ea86f4064560b8263bae24612f3d3eb5db9afccc
sandra-yani <sandra.april29@smk.belajar.id>
2025-04-29 19:26:30 +0700

    memperbaiki session dan sidemenu

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -1,21 +1,20 @@
 <?php
 session_start();
-// Ambil data dari sesi
-$userId = $_SESSION["pelanggan_id"];
+
+$userId = $_SESSION["user_id"];  
 $nama = $_SESSION["nama"];
-$kontak = $_SESSION["kontak"];
-// Ambil notifikasi jika ada, kemudian hapus dari sesi
+
 $notification = $_SESSION["notification"] ?? null;
 if ($notification) {
     unset($_SESSION['notification']);
 }
-/* Periksa apakah sesi username dan role sudah ada,
-jika tidak arahkan ke halaman login */
-if (empty($_SESSION["kontak"])) {
+
+if (empty($_SESSION["user_id"])) {  
     $_SESSION['notification'] = [
         'type' => 'danger',
         'message' => 'Silakan Login Terlebih Dahulu!'
     ];
-    header('Location: ./auth/login.php');
-    exit(); // Pastikan script berhenti setelah pengalihan
+    header('Location: ./auth/login.php');  
+    exit(); 
 }
+?>
