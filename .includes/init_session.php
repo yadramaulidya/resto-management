8072832a357737031ce873d56cdbde3813563849
sandra-yani <sandra.april29@smk.belajar.id>
2025-05-05 13:33:43 +0700

    memperbaiki sessi dan notif

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -1,5 +1,8 @@
 <?php
+// Pastikan sesi hanya dimulai jika belum aktif
+if (session_status() == PHP_SESSION_NONE) {
     session_start();
+}
 
 $userId = $_SESSION["user_id"] ?? null;  
 $nama = $_SESSION["nama"] ?? null; 
