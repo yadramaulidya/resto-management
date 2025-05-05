7fa25df1f32b661c85e8d3f8bee00979908da916
sandra-yani <sandra.april29@smk.belajar.id>
2025-05-05 15:04:16 +0700

    memperbaiki sessi

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -1,13 +1,14 @@
 <?php
-// Pastikan sesi hanya dimulai jika belum aktif
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
 $userId = $_SESSION["user_id"] ?? null;  
 $nama = $_SESSION["nama"] ?? null; 
+$role = $_SESSION["role"] ?? null;  
 
 $notification = $_SESSION["notification"] ?? null;
+
 if (empty($userId)) {
     $_SESSION['notification'] = [
         'type' => 'danger',
